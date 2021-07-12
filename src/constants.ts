c6d264e0e4735d00e45735862af829d6ad6f9e96
alexlee-dev <alex@alexlee.dev>
2021-07-11 20:46:38 -0700

    ✨ feat: ability to visit pages in account list

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { Account } from "./types";
+
 export const loginButtonSelector = 'button[type="submit"]';
 export const notNowRememberButtonSelector =
   "#react-root > section > main > div > div > div > div > button";
@@ -7,3 +9,16 @@ export const passwordFieldSelector =
   "#loginForm > div > div:nth-child(2) > div > label > input";
 export const usernameFieldSelector =
   "#loginForm > div > div:nth-child(1) > div > label > input";
+
+// Top 3 Accounts on Instagram
+export const accountList: Account[] = [
+  {
+    username: "instagram",
+  },
+  {
+    username: "cristiano",
+  },
+  {
+    username: "therock",
+  },
+];
