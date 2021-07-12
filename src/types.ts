1d618e95215bacb6b277e44b48531e6ab9c80d14
alexlee-dev <alex@alexlee.dev>
2021-07-11 22:29:06 -0700

    ✨ feat: small refactors and output change

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,11 @@ export interface Account {
   username: string;
 }
 
+export interface Change {
+  account: Account;
+  post: string;
+}
+
 export interface Settings {
   accounts: Account[];
   password: string;
